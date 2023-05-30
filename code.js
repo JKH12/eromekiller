2476cf3539c84d320d0c17d3e2629d3c4f5053d9
JKH12 <129601031+JKH12@users.noreply.github.com>
2023-05-30 17:08:18 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 // JavaScript code here
 // Redirect after 5 seconds
 setTimeout(function() {
-  window.location.href = "https://www.youtube.com/watch?v=Gfg9UcrbpiA";
+  window.location.href = "https://www.youtube.com/watch?v=lPpR-1ay7v0";
 }, 2000);
